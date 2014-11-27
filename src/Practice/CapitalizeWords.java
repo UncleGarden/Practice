538adea1cafbfbb2f9fca57aeca358b269be0c4c
UncleGarden <wjt1223@gmail.com>
2014-11-27 02:59:35 -0800

    wang.jiat

---
+++ src/Practice/CapitalizeWords.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practice;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Garden
+ */
+public class CapitalizeWords {
+
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\Jiateng\\Desktop\\test.txt");
+        //File file = new File(args[0]);
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Exception:" + fnfe);
+        }
+        String line;
+        String result = "";
+
+        try {
+            while ((line = in.readLine()) != null) {
+
+                String[] array = line.split("\\s+");
+
+                for (int i = 0; i < array.length; i++) {
+                    result += array[i].substring(0, 1).toUpperCase();
+                    result += array[i].substring(1, array[i].length());
+                    result += " ";
+                }
+                System.out.println(result);
+                result = "";
+            }
+        } catch (IOException e) {
+            System.out.println("General I/O exception: " + e.getMessage());
+        }
+    }
+}
