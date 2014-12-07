bada6dbcdae130fe17305e94b8a1c40a59b585e0
UncleGarden <wjt1223@gmail.com>
2014-12-06 19:39:41 -0800

    wang.jiat

---
+++ src/Practice/LongestCommonSubsequence.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practice;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Garden
+ */
+public class LongestCommonSubsequence {
+
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\Jiateng\\Desktop\\test.txt");
+        //File file = new File(args[0]);
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Exception:" + fnfe);
+        }
+        String line;
+
+        try {
+            while ((line = in.readLine()) != null) {
+
+                if (line.isEmpty()) {
+                    continue;
+                }
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("General I/O exception: " + e.getMessage());
+        }
+    }
+}
