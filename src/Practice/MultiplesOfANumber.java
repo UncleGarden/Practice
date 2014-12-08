a5af96174e5a7023eefc1f6bbd8382bdba12885e
UncleGarden <wjt1223@gmail.com>
2014-12-08 13:20:36 -0800

    wang.jiat

---
+++ src/Practice/MultiplesOfANumber.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practice;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Garden
+ */
+public class MultiplesOfANumber {
+
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\Jiateng\\Desktop\\test.txt");
+        //File file = new File(args[0]);
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Exception:" + fnfe);
+        }
+        String line;
+
+        try {
+            while ((line = in.readLine()) != null) {
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                String[] array = line.split(",");
+
+                int x = Integer.valueOf(array[0]);
+                int n = Integer.valueOf(array[1]);
+                int result = n;
+
+                while (result < x) {
+                    result += n;
+                }
+
+                System.out.println(result);
+            }
+        } catch (IOException e) {
+            System.out.println("General I/O exception: " + e.getMessage());
+        }
+    }
+}
