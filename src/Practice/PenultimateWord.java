899b85239eb93e8bd312af4b24ab28e054a282b3
UncleGarden <wjt1223@gmail.com>
2014-12-15 02:08:29 -0800

    wang.jiat

---
+++ src/Practice/PenultimateWord.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practice;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Garden
+ */
+public class PenultimateWord {
+
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\Jiateng\\Desktop\\test.txt");
+        //File file = new File(args[0]);
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Exception:" + fnfe);
+        }
+        String line;
+
+        try {
+            while ((line = in.readLine()) != null) {
+
+                String[] array = line.split("\\s+");
+
+                
+                System.out.println(array[array.length - 2]);
+
+            }
+        } catch (IOException e) {
+            System.out.println("General I/O exception: " + e.getMessage());
+        }
+    }
+}
